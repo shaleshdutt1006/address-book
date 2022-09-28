4e45e0db23eebfda14f787b3618442c023bfbc4e
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-09-28 11:31:26 +0530

    Added: Create Contact in address book with information provided

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,10 +1,30 @@
 package com.addressbook;
 
+import java.util.ArrayList;
+
 public class AddressBook {
 
+    ArrayList<Person> addressbook = new ArrayList<Person>();
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to address book program");
-
+    public ArrayList<Person> createContact(Person person) {
+        this.addressbook.add(person);
+        return this.addressbook;
     }
+
+    public void printAddressBook() {
+        for (Person person : this.addressbook) {
+
+            System.out.println("-----------------------------------------------------");
+            System.out.println("First name = " + person.getFirstName());
+            System.out.println("last name = " + person.getLastName());
+            System.out.println("address = " + person.getAddress());
+            System.out.println("city = " + person.getCity());
+            System.out.println("state = " + person.getState());
+            System.out.println("zip = " + person.getZip());
+            System.out.println("phone number = " + person.getPhoneNumber());
+            System.out.println("-----------------------------------------------------");
+        }
+    }
+
 }
+
