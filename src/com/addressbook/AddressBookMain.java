8072c4f9422530a297911d3b634ce5409ff81ac5
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-01 15:53:59 +0530

    Added: Count number of Contacts in same city or state in multiple addressbooks

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -50,14 +50,14 @@ public class AddressBookMain {
     */
 
         AddressBook addressBook = new AddressBook();
-        addressBook.addContact(new Person("amit", "dutt", "bandra", "mumbai", "haryana", 54654, "845165489165"));
+        addressBook.addContact(new Person("sunita", "sharma", "bandra", "mumbai", "haryana", 546854, "85489165"));
         addressBook.addContact(person2);
         AddressBook addressBook1 = new AddressBook();
         addressBook1.addContact(person3);
         addressBook1.addContact(person4);
         AddressBook addressBook2 = new AddressBook();
         addressBook2.addContact(person5);
-        addressBook2.addContact(person4);
+        addressBook2.addContact(person1);
 
 
         Map<String, AddressBook> map = new HashMap<>();
@@ -72,13 +72,19 @@ public class AddressBookMain {
 
         System.out.println("Enter city you want contacts for");
         String city = scanner.next();
+        System.out.println("Enter state you want contacts for");
+        String state = scanner.next();
     /*
     for iterating the map we take a variable e  and iterating over every addressbook in the map and temp is a variable of
     type AddressBook like we take int a. In temp we are getting values of addressbooks one by one from map further for-each
     loop to iterate persons in the addressbook and if person city equal to the input city then printing it.
      */
 
-        System.out.println("Contacts of person in the same city are ");
+
+        int count1 = 0;
+        int count2 = 0;
+
+
         for (Map.Entry<String, AddressBook> e : map.entrySet()) {
 
             AddressBook temp = e.getValue();
@@ -86,15 +92,21 @@ public class AddressBookMain {
             for (Person person : temp.getAddressbook()) {
 
                 if (person.getCity().equalsIgnoreCase(city)) {
+                    System.out.println("Contacts of person in the same city are ");
+                    count1++;
+                    temp.printAddressBook(person);
 
+                } else if (person.getState().equalsIgnoreCase(state)) {
+                    System.out.println("Contacts of person in the same state are ");
+                    count2++;
                     temp.printAddressBook(person);
 
                 }
             }
 
         }
-
-
+        System.out.println("Number of persons in the same city in addressbook are : " + count1);
+        System.out.println("Number of persons in the same state in addressbook are : " + count2);
     }
 }
 
